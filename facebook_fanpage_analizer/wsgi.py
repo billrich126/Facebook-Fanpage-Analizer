a7999e4c02c325c45c4ad9d3fc699fe0bee6b7dd
jakubtustanowski <tusty53@gmail.com>
2017-05-24 23:33:34 +0200

    psycopg2==2.7.1

--- facebook_fanpage_analizer/wsgi.py
+++ facebook_fanpage_analizer/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "facebook_fanpage_analizer.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
